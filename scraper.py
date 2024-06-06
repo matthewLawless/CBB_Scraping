6fbbed79243262c178a86deeea9357e13081bf8b
matthewLawless <mjlawless04@gmail.com>
2024-06-05 21:41:34 -0500

    Clicking buttons
    Found buttons to click the drop down for the calendar and to go to the previous month. Able to fetch the text from the divs to get the current month and year. Next need to go back to start of 2023-2024 season to begin parsing from there and look for divs with data.

--- scraper.py
+++ scraper.py
@@ -5,21 +5,40 @@ import sys
 from PyQt5.QtGui import QGuiApplication
 from PyQt5.QtCore import QUrl
 from PyQt5.QtWebEngineWidgets import QWebEnginePage
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
 
 fake_URL = "https://realpython.github.io/fake-jobs/"
 URL = "https://www.vegasinsider.com/college-basketball/odds/las-vegas/"
 
+months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
 fake_Page = requests.get(fake_URL)
 page = requests.get(URL)
 
-print(fake_Page.text)
+sPage = webdriver.Chrome()
+sPage.maximize_window()
+sPage.get(URL)
+
+dropDownForCalendar = sPage.find_element(By.CLASS_NAME, "down")
+dropDownForCalendar.click()
+previousCalendarButton = sPage.find_element(By.CLASS_NAME, "prev")
+previousCalendarButton.click()
+previousCalendarButton.click()
+currentMonth = sPage.find_element(By.CLASS_NAME, "current")
+print("currentMonth ==> ", currentMonth.text)
+
+#print(fake_Page.text)
 print("====================")
 session = HTMLSession()
 
 r = session.get(URL)
 r.html.render(sleep=1)
 
-print(r.status_code)
+
+
+#print(r.status_code)
 
 #class Client(QWebEnginePage):
 #    def __init__(self, url):
