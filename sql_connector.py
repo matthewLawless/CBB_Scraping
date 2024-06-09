aedf5c3d230ad7e9f13f2ab5686a205757bd6eff
matthewLawless <mjlawless04@gmail.com>
2024-06-09 12:58:16 -0500

    Figured out how to insert dates
    I think I've done everything I need to do with the database for now. Next step is to actually go through and parse a day's worth of games into the database. Also need to put database connection code in a different file which we do not commit to github and change the password to the database.

--- sql_connector.py
+++ sql_connector.py
@@ -1,4 +1,6 @@
 import mysql.connector
+import datetime
+from datetime import datetime
 
 cbb_betting_lines = mysql.connector.connect(
     host = "localhost",
@@ -8,4 +10,20 @@ cbb_betting_lines = mysql.connector.connect(
 )
 
 cursor = cbb_betting_lines.cursor()
-cursor.execute("SELECT date FROM moneyline")
+cursor.execute("SELECT * FROM moneyline")
+ans = cursor.fetchall()
+
+for i in ans:
+    print(i)
+
+#date = datetime.date(2024, 6, 9)
+date = datetime.now()
+#date = strftime(date)
+print(date)
+
+# cursor.execute(("""INSERT INTO moneyline(date) 
+#                             VALUES ('2012-10-03')"""))
+cursor.execute("""UPDATE moneyline
+                SET home = "MSU", away = "UAB"
+                WHERE id = 7""")
+cbb_betting_lines.commit()
